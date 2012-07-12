30da24004b8b6fbb098fe7d344735f43b04c71ab
zhoumengchun <zhoumengchun@sina.com>
2012-07-12 07:16:48 +0000

    Read添加泛型类型处理支持

--- xyz.mongo.core/src/java/main/xyz/mongo/Read.java
+++ xyz.mongo.core/src/java/main/xyz/mongo/Read.java
@@ -11,4 +11,10 @@ import java.lang.annotation.Target;
 @Documented
 public @interface Read {
      String name() default "one";
+     /**
+      * 设置一些泛型类型，比如List之后的类型，由于java
+      * 泛型的擦拭实现，导致这些东西不好得到，所以，必须在此添加
+      * @return
+      */
+     Class ctype() default Object.class;
 }
